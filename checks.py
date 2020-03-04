73ccd35442965b4d37a2e6e52c004f31c4b47f0d
Stanislav Pidhorskyi <stanislav@podgorskiy.com>
2020-03-04 00:10:15 -0500

    Python module for computing sifts

---
+++ checks.py
@@ -0,0 +1,63 @@
+import siftgpu
+import imageio as io
+import numpy as np
+from PIL import Image, ImageDraw
+
+x = io.imread('IMG_2759.JPG')
+
+b = siftgpu.Bitmap(x[:, :, ::-1])
+
+print(b.is_grey())
+
+b.write('out.jpg')
+
+b.clone_as_grey().write('out_g.jpg')
+
+options = siftgpu.make_default_quality_options()
+
+extractor = siftgpu.SiftExtractor(options)
+
+descriptors, keypoints = extractor.extract_sift(b.clone_as_grey())
+
+for keypoint in keypoints:
+    print(keypoint.x)
+    print(keypoint.y)
+    print(keypoint.compute_scale())
+    print(keypoint.compute_orientation())
+
+print(descriptors)
+print(descriptors.shape)
+
+image = Image.fromarray(x.copy())
+draw = ImageDraw.Draw(image)
+
+
+for keypoint in keypoints:
+    point_x = keypoint.x
+    point_y = keypoint.y
+
+    draw.line((point_x - 4, point_y - 4, point_x + 4, point_y + 4), fill=(255, 50, 50), width=2)
+    draw.line((point_x - 4, point_y + 4, point_x + 4, point_y - 4), fill=(255, 50, 50), width=2)
+
+io.imwrite('sift.jpg', image)
+
+image = Image.fromarray(x.copy())
+draw = ImageDraw.Draw(image)
+
+extractor.set_keypoints(keypoints[:100], False)
+descriptors_new, keypoints = extractor.extract_sift(b.clone_as_grey())
+
+for keypoint in keypoints:
+    point_x = keypoint.x
+    point_y = keypoint.y
+
+    draw.line((point_x - 4, point_y - 4, point_x + 4, point_y + 4), fill=(255, 50, 50), width=2)
+    draw.line((point_x - 4, point_y + 4, point_x + 4, point_y - 4), fill=(255, 50, 50), width=2)
+
+assert len(keypoints) == 100
+assert descriptors_new.shape[0] == 100
+
+io.imwrite('sift_from_keypoints.jpg', image)
+
+print(descriptors[:100])
+print(descriptors_new)
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+setup(
+    name='siftgpu',
+    version='0.0.1',
+    description='siftgpu',
+    long_description='siftgpu',
+    url='',
+    author='Stanislav Pidhorskyi',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    data_files=[('', ['siftgpu.so'])]
+)
